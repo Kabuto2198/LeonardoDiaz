974a25c1fbd533d5f36dda0dadfc2db75ff5d16b
kabuto2198 <cldiaz12@misena.edu.co>
2022-11-17 19:41:33 -0500

    39

--- 39/src/app/Notificacion/Email.php
+++ 39/src/app/Notificacion/Email.php
@@ -1,5 +1,6 @@
 <?php
 
+namespace App\Notificacion;
 class Email
 {
 
--- 39/src/app/PaymentGateway/Paddle/Transaction.php
+++ 39/src/app/PaymentGateway/Paddle/Transaction.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 
-namespace PaymentGateway\Paddle;
+namespace App\PaymentGateway\Paddle;
 
 use PaymentGateway\Stripe\Transaction as PaddleTransaction;
 
